9374a3768ae4966136d9b7d48209cf9e3aa08af6
hignz <higz-14@live.com>
2018-08-06 15:21:38 +0100

    Adding functionality to options page

--- options/options.js
+++ options/options.js
@@ -1,1 +1,7 @@
-//TODO - A lot
+window.addEventListener('load', function (){
+    options.playSound.checked = JSON.parse(localStorage.playNotificationSound)
+
+    options.playSound.onchange = function () {
+        localStorage.playNotificationSound = options.playSound.checked;
+    };
+});
