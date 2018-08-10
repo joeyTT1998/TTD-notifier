04f2a68b497eed49ee0f6e5e255fbf6c84eeadd7
hignz <higz-14@live.com>
2018-08-10 03:35:13 +0100

    Fixed options.html functionality

--- options/options.js
+++ options/options.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', function (){
-    options.playSound.checked = JSON.parse(localStorage.playNotificationSound)
-
-    options.playSound.onchange = function () {
-        localStorage.playNotificationSound = options.playSound.checked;
-    };
+    options.playSound.checked = JSON.parse(localStorage.playNotificationSound);
+    volumeRange.value = JSON.parse(localStorage.notificationVol);
+    volumeLabel.value = JSON.parse(localStorage.notificationVol);
 });
+
+options.playSound.onchange = function () {
+    localStorage.playNotificationSound = options.playSound.checked; 
+};
+
+volumeRange.onchange = function() {
+    localStorage.notificationVol = volumeRange.value;
+    volumeLabel.value = volumeRange.value;
+}
