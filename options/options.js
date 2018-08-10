07011c4f193dbe9893bf3d0d1374e9f3daa66832
hignz <higz-14@live.com>
2018-08-10 14:39:06 +0100

    Ability to change icon(dark or light) for people using dark chrome themes

--- options/options.js
+++ options/options.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function (){
     options.playSound.checked = JSON.parse(localStorage.playNotificationSound);
     volumeRange.value = JSON.parse(localStorage.notificationVol);
     volumeLabel.value = JSON.parse(localStorage.notificationVol);
+    document.getElementById("iconTheme").selectedIndex = localStorage.iconThemeDark;
 });
 
 options.playSound.onchange = function () {
@@ -12,3 +13,15 @@ volumeRange.onchange = function() {
     localStorage.notificationVol = volumeRange.value;
     volumeLabel.value = volumeRange.value;
 }
+
+document.getElementById("iconTheme").addEventListener("change", function(){
+    let iconPath = this.selectedIndex == 0 ? "/icon/128_Black.png" : "/icon/128_White.png";
+
+    chrome.browserAction.setIcon({
+        "path": iconPath
+    });
+
+    localStorage.iconThemeDark = this.selectedIndex;
+
+    console.log(localStorage.iconThemeDark);
+});
