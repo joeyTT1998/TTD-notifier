122305d85c0431caf31631ec59cedcfd89983594
hignz <higz-14@live.com>
2018-08-11 01:11:20 +0100

    Added close button to options page

--- options/options.js
+++ options/options.js
@@ -1,27 +1,33 @@
-window.addEventListener('load', function (){
+window.addEventListener('load', function () {
     options.playSound.checked = JSON.parse(localStorage.playNotificationSound);
     volumeRange.value = JSON.parse(localStorage.notificationVol);
     volumeLabel.value = JSON.parse(localStorage.notificationVol);
-    document.getElementById("iconTheme").selectedIndex = localStorage.iconThemeDark;
+    document.getElementById("iconTheme").selectedIndex = localStorage.iconTheme;
+});
+
+document.getElementById("btnClose").addEventListener("click", function () {
+    window.close();
 });
 
 options.playSound.onchange = function () {
-    localStorage.playNotificationSound = options.playSound.checked; 
+    localStorage.playNotificationSound = options.playSound.checked;
+
+    chrome.contextMenus.update("notifSound", {
+        checked: JSON.parse(localStorage.playNotificationSound),
+    });
 };
 
-volumeRange.onchange = function() {
+volumeRange.oninput = function () {
     localStorage.notificationVol = volumeRange.value;
     volumeLabel.value = volumeRange.value;
 }
 
-document.getElementById("iconTheme").addEventListener("change", function(){
+document.getElementById("iconTheme").addEventListener("change", function () {
     let iconPath = this.selectedIndex == 0 ? "/icon/128_Black.png" : "/icon/128_White.png";
 
     chrome.browserAction.setIcon({
         "path": iconPath
     });
 
-    localStorage.iconThemeDark = this.selectedIndex;
-
-    console.log(localStorage.iconThemeDark);
+    localStorage.iconTheme = this.selectedIndex;
 });
