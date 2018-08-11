c8461593d76e795a92d888aae0185d05c818da33
hignz <higz-14@live.com>
2018-08-11 01:10:44 +0100

    Cleanup

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,19 @@
 document.getElementById("options").addEventListener("click", openOptions);
 
-var getLastSeen = function() {
+var getLastSeen = function () {
     let hours = ((new Date().getTime() - (new Date(localStorage.lastSeen)).getTime()) / (1000 * 60 * 60));
     let mins = (hours * 10) * 6 + 3;
 
-    if(hours < 1.0){
+    if (hours < 1.0) {
         return Math.round(mins) + "m ago";
-    }
-    else if(hours >= 24){
+    } else if (hours >= 24) {
         return "~" + Math.floor(hours / 24) + "d ago";
-    }
-    else {
-        return Math.round(hours)+ "h ago";
+    } else {
+        return Math.round(hours) + "h ago";
     }
 }
 
-var updateOnlineStatus = function() {
+var updateOnlineStatus = function () {
     if (JSON.parse(localStorage.isLive)) {
         document.getElementById("last-seen").style.display = "none";
     } else {
@@ -24,7 +22,7 @@ var updateOnlineStatus = function() {
     }
 }
 
-function openOptions(){
+function openOptions() {
     chrome.runtime.openOptionsPage();
 }
 
